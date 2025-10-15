6f99c58cf0bc4b91e7c40f3e437fa39912e1a947
hoehoeabi <0203jyh00@naver.com>
2025-10-15 22:00:56 +0900

    Feat(verification): Verification 구현체 추가
    - 입력값 형식, 커스텀 구분자, 숫자 구분자 검증을 수행하는 클래스입니다.
    - Validators를 생성자 주입받아 내부 Validator들을 활용합니다.

---
+++ src/main/java/calculator/verification/VerificationImpl.java
@@ -0,0 +1,30 @@
+package calculator.verification;
+
+import calculator.validation.ValidatorsImpl;
+
+import java.util.Set;
+
+public class VerificationImpl implements Verification {
+
+    private final ValidatorsImpl validators;
+
+    public VerificationImpl(ValidatorsImpl validators) {
+        this.validators = validators;
+    }
+
+    @Override
+    public boolean validateInput(String input, String regex) {
+        return validators.getInputFormatValidator().validate(input, regex);
+    }
+
+
+    @Override
+    public boolean areCustumDelimiters(String input, Set<Character> delimiters) {
+        return validators.getDelimiterCharacterValidator().validate(input, delimiters);
+    }
+
+    @Override
+    public boolean haveNumberDelimiters(char start, char end, Set<Character> delimiters) {
+        return validators.getNumberDelimiterValidator().validate(start, end, delimiters);
+    }
+}
