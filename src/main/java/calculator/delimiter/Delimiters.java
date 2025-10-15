386e48fbee2ec09288a5e5d988b17716cacb637f
hoehoeabi <0203jyh00@naver.com>
2025-10-15 23:45:36 +0900

    feat(Delimitlers): 구분자 상태 관리를 위한 싱글톤 구현
    애플리케이션의 여러 컴포넌트에서 구분자 목록을 공유하고
    동적으로 수정하기 위해 싱글톤 패턴을 적용합니다.
    
    - 검증, 파싱, 계산 로직에서 동일한 구분자 Set을 참조
    - 사용자 정의 구분자를 동적으로 추가 및 공유 가능
    - 기본 구분자로 ','와 ':'를 설정하여 초기화

---
+++ src/main/java/calculator/delimiter/Delimiters.java
@@ -0,0 +1,31 @@
+package calculator.delimiter;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Delimiters {
+
+    private static Delimiters delimiters;
+
+    private Set<Character> delimitersSet = new HashSet<>(Arrays.asList(',', ':'));
+
+    private Delimiters() {}
+
+    public static Delimiters getInstance() {
+        if (delimiters == null) {
+            delimiters = new Delimiters();
+        }
+
+        return delimiters;
+    }
+
+    public Set<Character> getDelimitersSet() {
+        return delimitersSet;
+    }
+
+    public void add(char delimiter) {
+        delimitersSet.add(delimiter);
+    }
+
+}
