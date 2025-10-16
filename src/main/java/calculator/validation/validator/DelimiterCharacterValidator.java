a3e3214e4b23c6fa8428e6e2ea33c07aba4c3839
hoehoeabi <0203jyh00@naver.com>
2025-10-16 22:47:47 +0900

    Refactor : 문자열에 남아있는 문자들에서 구분자가 아닌지 검증하는 로직 변경
    - 기존 로직: 숫자이거나 문자인경우만 걸러서 특수문자는 잡지 못하였습니다.
    - 변경된 로직 : 구분자에 등록되지 않은 문자가 숫자가 아닐경우 false를 반환합니다.

--- src/main/java/calculator/validation/validator/DelimiterCharacterValidator.java
+++ src/main/java/calculator/validation/validator/DelimiterCharacterValidator.java
@@ -10,14 +10,12 @@ public class DelimiterCharacterValidator {
 
         Set<Character> delimiters = Delimiters.getInstance().getDelimitersSet();
         for(char c : input.toCharArray()) {
-            // 문자이며 구분자에 속해있으면 통과
-            if(Character.isLetter(c) && delimiters.contains(c)) continue;
-            // 문자이며 구분자에 속해있지 않으면 실패
-            else if(Character.isLetter(c) && !delimiters.contains(c)) return false;
-            // 숫자이며 구분자에 속해있으면 통과
-            else if(Character.isDigit(c) && delimiters.contains(c)) continue;
-            // 숫자이며 구분자에 속해있지 않으면 통과
-            else continue;
+            boolean isDigit = Character.isDigit(c);
+            boolean isRegisteredDelimiter = delimiters.contains(c);
+
+            if (!isRegisteredDelimiter && !isDigit) {
+                return false;
+            }
 
         }
         return true;
