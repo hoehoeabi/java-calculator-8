10d4e6daad074b62eb9927865c974a9ae7e7d744
hoehoeabi <0203jyh00@naver.com>
2025-10-16 22:18:58 +0900

    test(단위): 유틸 패키지 단위 테스트 코드 작성
    유틸패키지안에 있는 두 메서드에 대해 단위테스트를 작성했습니다.
    
    - 사용자 입력값에서 잘 구분자를 추가하는지
    - 문자열에서 숫자들만 잘 골라 계산하는지

---
+++ src/test/java/calculator/util/UtilTest.java
@@ -0,0 +1,40 @@
+package calculator.util;
+
+import calculator.delimiter.Delimiters;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UtilTest {
+
+    @Test
+    void 문자열에서_숫자들_계산(){
+        // given
+        Set<Character> delimiters = new HashSet<>(Arrays.asList(',',':'));
+        String input = "11,2:3";
+
+        // when & then
+        assertThat(StringCalculator.sum(input)).isEqualTo(16);
+    }
+
+    @Test
+    void 입력값에서_구분자_추가(){
+        // given
+        Set<Character> delimiters = Delimiters.getInstance().getDelimitersSet();
+        String input = "//@\\n//#\\n11,2:3";
+
+        // when
+        StringParser.addDelimiter(input);
+
+        // then
+        assertThat(delimiters.size()).isEqualTo(4);
+    }
+
+}
