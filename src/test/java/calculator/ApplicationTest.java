6f73439b8b4e25ff155ab041b6dec2230e4ef7ab
hoehoeabi <0203jyh00@naver.com>
2025-10-16 20:54:45 +0900

    test(통합): 메인클래스 통합테스트 작성
    여라가지 입력 갑에 대헤 성공 혹은 실패여부를 검사합니다.

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,23 +9,89 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 커스텀_구분자_사용() {
+    void 커스텀_구분자_사용_성공() {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run("//;\\n//!\\n//@\\n1!22@3,4");
+            assertThat(output()).contains("결과 : 30");
         });
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    void 커스텀_구분자_미사용_성공() {
+        assertSimpleTest(() -> {
+            run("11");
+            assertThat(output()).contains("결과 : 11");
+        });
+    }
+
+    @Test
+    void 공백_문자열_입력시_0을_출력_성공() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_등록구문만_입력(){
+        assertSimpleTest(() -> {
+            run("//@\\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    // 예외 시나리오
+    @Test
+    void 예외_테스트_구분자가_첫글자_실패() {
+        assertSimpleTest(() -> {
+            run(",1,2,3");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
+
+    @Test
+    void 예외_테스트_구분자가_마지막글자_실패() {
+        assertSimpleTest(() -> {
+            run("1,2,");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
+
+    @Test
+    void 예외_테스트_등록되지않은_구분자가_마지막글자_실패() {
+        assertSimpleTest(() -> {
+            run("1,2?");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
+
+    @Test
+    void 예외_테스트_등록되지않은_구분자가_처음글자_실패() {
+        assertSimpleTest(() -> {
+            run("?1,2,");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
+
+    @Test
+    void 예외_테스트_구분자사이_숫자없음_실패(){
+        assertSimpleTest(() -> {
+            run("1,,2");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
+
+    @Test
+    void 예외_테스트_숫자구분자가_마지막에옴_실패(){
+        assertSimpleTest(() -> {
+            run("//4\\n1,2,3,4");
+            assertThat(output()).contains("[ERROR]");
+        });
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
